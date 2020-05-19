a51b08e9f3880c3039f9e2fee65836a2efe7d224
oldlu <112729004@qq.com>
2020-05-19 17:36:44 +0800

    @到ev 删除license依赖

--- zipkin-server/start.sh
+++ zipkin-server/start.sh
@@ -1,1 +1,7 @@
 java -DES_INDEX_SHARDS=2 -DSTORAGE_TYPE=elasticsearch -DES_INDEX=zipkin_test2 -DES_HOSTS=http://10.201.211.146,http://10.201.211.147 -jar target/zipkin*.jar
+
+
+./mvnw -DskipTests --also-make -pl zipkin-server clean install
+./mvnw -DskipTests --also-make -pl clean install
+
+mvn clean package  -Dmaven.test.skip=true
